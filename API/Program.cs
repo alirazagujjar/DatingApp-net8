21713c5b6b5ae55d598e0471d8ad9da4618a7c2c
Ali Raza Musharaf <ali.raza.musharaf@vaival.com>
2024-07-05 16:03:25 +0500

    new changes

--- API/Program.cs
+++ API/Program.cs
@@ -13,7 +13,13 @@ builder.Services.AddDbContext<DataContext>(opt=>
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddCors(options =>
+        {
+            options.AddPolicy("AllowSpecificOrigin",
+                builder => builder.WithOrigins("http://localhost:4200")
+                                  .AllowAnyHeader()
+                                  .AllowAnyMethod());
+        });
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -23,7 +29,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-
+app.UseCors("AllowSpecificOrigin");
 app.MapControllers();
 
 app.Run();
