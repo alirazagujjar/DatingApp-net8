63809a5411b001103a4a6a2f73faa5a7605bb577
Ali Raza Musharaf <ali.raza.musharaf@vaival.com>
2024-07-25 17:43:10 +0500

    Endof section 6

--- Client/src/app/app.routes.ts
+++ Client/src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
+import { MemberDetailsComponent } from './members/member-details/member-details.component';
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
+import { authGuard } from './_guards/auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'',component:HomeComponent},
+    {
+        path:'',
+        runGuardsAndResolvers:'always',
+        canActivate:[authGuard],
+        children:[
+            {path:'members',component:MemberListComponent , canActivate:[authGuard]},
+            {path:'members/:id',component:MemberDetailsComponent,canActivate:[authGuard]},
+            {path:'lists',component:ListsComponent,canActivate:[authGuard]},
+            {path:'messages',component:MessagesComponent,canActivate:[authGuard]}
+        ]
+
+    },
+    
+    {path:'**',component:HomeComponent,pathMatch:'full'}
+];
--- Client/src/app/nav/nav.component.ts
+++ Client/src/app/nav/nav.component.ts
@@ -2,29 +2,40 @@ import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../_services/account.service';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { TitleCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [FormsModule, BsDropdownModule],
+  imports: [FormsModule, BsDropdownModule,RouterLink,RouterLinkActive,TitleCasePipe],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
 export class NavComponent {
   accountService = inject(AccountService);
+  private routes= inject(Router);
+  private toastr = inject(ToastrService);
+  isLogin :boolean=false;
   maodel: any = {};
   login() {
     this.accountService.login(this.maodel).subscribe({
-      next: (response: any) => {
-        console.log(response);
-
-
+      next: _ => {
+       this.isLogin=true;
+        this.routes.navigateByUrl('/members');
       },
-      error: (error: any) => console.log(error)
+      error: (error: any) => {
+        console.log(error)
+        this.toastr.error(error.error.messgae);
+      }
+      
 
     });
   }
   logout() {
     this.accountService.logout();
+    this.isLogin=false;
+    this.routes.navigateByUrl('/');
   }
 }
